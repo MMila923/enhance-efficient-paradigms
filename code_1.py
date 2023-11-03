28a69fbf3fa9028f0f0f8001a6ca50d1f46def64
Mila <m9234434@gmail.com>
2023-11-03 13:13:04 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,50 +1,42 @@
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-}
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-        }
-        for (int item : data) {
+def main():
+import random
+
+    for item in data:
+    return data
 
 
-    }
-    public static void main(String[] args) {
-        }
-if __name__ == "__main__":
 
         print(f"Random Number: {item}")
-    main()
-    return data
-def main():
     data = generate_random_data()
-    for item in data:
-
-
+    main()
+if __name__ == "__main__":
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]        print(f"Random Number: {item}")
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
+
 import random
-def generate_random_data():        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
+    main()
+def generate_random_data():
+if __name__ == "__main__":
 
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
-}
-        for (int item : data) {
-        }
+
+
+    for item in data:
+def main():
+    data = generate_random_data()main();
+        console.log("Random Number: " + item);
     }
 
-    public static void main(String[] args) {
-public class RandomDataGenerator {        for (int i = 0; i < 10; i++) {
-
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int item : data) {
-
 }
-        }
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-        int[] data = new int[10];
-        }
-    }
-            System.out.println("Random Number: " + item);
+function generateRandomData() {
+    data.forEach(item => {
+function main() {
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+
+    const data = generateRandomData();
+}
+    const data = [];
+    });
